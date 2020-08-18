129b92464e39e4f4c3d0e7b5d6f3c3e536aa35b7
finako <57268366+finako@users.noreply.github.com>
2020-08-18 17:12:16 +0200

    Add example settings.py file

---
+++ example_settings.py
@@ -0,0 +1,6 @@
+apiKey="Your key here" #your OAuth-Token for the Dropbox API
+tmpFileEnding=".tmpROOM" #file ending for local tmp files, ROOM will be replaced with the number of the chat-room
+chatFileEnding=".chatROOM" #file ending for chat files on DropBox, ROOM will be replaced with the number of the chat-room
+tmpFolder="chat_data/" #folder location for all tmp- and chat-files
+dropboxDestination="" #where to write on dropbox, "" is for the root / -folder
+standardChatRoom=1 #standard chat room to enter
